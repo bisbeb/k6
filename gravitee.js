040fe7117ffe7c83ae3868524e882f250794d54f
bisbeb <linuxsatellite@gmail.com>
2022-08-28 22:31:48 +0200

    adds html report

--- gravitee.js
+++ gravitee.js
@@ -1,7 +1,19 @@
+import { htmlReport } from "https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js";
 import http from 'k6/http';
 import { sleep } from 'k6';
 
 export default function () {
-  http.get('http://localhost:8082/ccb/e2e');
+  const options = {
+    headers: {
+      'X-Gravitee-Api-Key': '4006ac0c-a567-44cf-94b2-17ee10ada475',
+    },
+  };
+  http.get('http://localhost:8082/ccb/e2e', options);
   sleep(1);
+}
+
+export function handleSummary(data) {
+  return {
+    "summary.html": htmlReport(data),
+  };
 }
